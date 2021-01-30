47fcc40f9944e5db011fcb1e136176419f84d2b1
Your Name <you@example.com>
2021-01-31 03:30:47 +0900

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%+@pmw3vt7&jmcsar=qq)%3j3iqxa7k6(vd_%108gjp3u426ex'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
